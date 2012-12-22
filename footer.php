d82271da4bfea674df4fc70f0dbfb8f89259aae9
Walmik <walmik.deshpande@gmail.com>
2012-12-21 16:15:22 -0800

    added pre style to code as well

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer" class="clearfix">
-		<p>All drawings/caricatures on this site and Wordpress theme design by <a href="http://walmik.info">Walmik</a>. <br />  WP responsive framework by <a href="http://www.elmastudio.de/wordpress-themes/">Elmastudio</a></p>
+		<p>All drawings/caricatures on this site by <a href="http://rigved.info">Rigved</a>  and Wordpress theme design by <a href="http://walmik.info">Walmik</a>. <br />  WP responsive framework by <a href="http://www.elmastudio.de/wordpress-themes/">Elmastudio</a></p>
 </div>
 <!--end Footer-->
 
