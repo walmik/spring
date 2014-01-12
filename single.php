a02769606fe1f83c07f9262bdd83b20bb3705291
Walmik Deshpande <walmik.deshpande@gmail.com>
2014-01-12 15:03:37 -0800

    Added tweet post button

--- single.php
+++ single.php
@@ -41,8 +41,10 @@
 				<?php endif; ?>
 				<?php comments_popup_link( __( 'Leave a comment', 'ari' ), __( '1 comment', 'ari' ), __( '% comments', 'ari' ) ); ?>
 				<?php edit_post_link( __( 'Edit &rarr;', 'ari' ), '| ', '' ); ?></p>
-
-
+<!-- tweet -->
+				<a href="https://twitter.com/share" class="twitter-share-button" data-via="_walmik" data-count="none">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<!-- /tweet-->
 				<?php comments_template( '', true ); ?>
 
 <?php endwhile; // end of the loop. ?>
