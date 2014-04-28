94da7ec0a2cf3b46488827cbae4f556ad3d05a67
Walmik <walmik.deshpande@gmail.com>
2014-04-27 21:34:25 -0700

    update ga code

--- header.php
+++ header.php
@@ -32,20 +32,16 @@
 	<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 	<?php wp_head(); ?>
 	
-	<script type="text/javascript">
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-8347688-7']);
-	_gaq.push(['_trackPageview']);
-      
-	(function() {
-	  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	  //ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	  ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-	  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-      
-      </script>
+  ga('create', 'UA-8347688-7', 'walmik.com');
+  ga('send', 'pageview');
+
+</script>
 	
 </head>
 
